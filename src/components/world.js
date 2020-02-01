91ddcb3e6d62271607d8fd5976b00ae0f1915176
Viljami Peltola <viljami.peltola@iki.fi>
2020-02-01 16:45:44 +0200

    Hide water.

--- src/components/world.js
+++ src/components/world.js
@@ -13,7 +13,12 @@ import createCar from './car';
 
 export default (engine) => {
   const maxSpeed = 0.2
-  const water = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+  const water = Bodies.rectangle(400, 1000, 810, 60, {
+    isStatic: true,
+    render: {
+      visible: false
+    }
+  });
 
   World.add(engine.world, [water]);
 
