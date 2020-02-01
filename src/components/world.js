078d0d4c9ff0be0d7b294fd740d4585c9fa713db
Viljami Peltola <viljami.peltola@iki.fi>
2020-02-01 17:00:09 +0200

    Make water larger to prevent falling infinitely.

--- src/components/world.js
+++ src/components/world.js
@@ -13,7 +13,7 @@ import createCar from './car';
 
 export default (engine) => {
   const maxSpeed = 0.2
-  const water = Bodies.rectangle(400, 1000, 810, 60, {
+  const water = Bodies.rectangle(1000, 1000, 3000, 20, {
     isStatic: true,
     render: {
       visible: false
