ddb83ae967f1d5ced254f2055f9db82a53cd29da
Viljami Peltola <viljami.peltola@iki.fi>
2020-02-01 19:44:19 +0200

    Move levels to own file. Add hill level.

---
+++ src/components/levels.js
@@ -0,0 +1,59 @@
+export default [
+  { // simple bridge
+    objects: [
+      { type: "start", x: 50, y: 50 },
+      { type: "ground", x: 160, y: 400, width: 400, height: 20 },
+      { type: "ground", x: 700, y: 400, width: 350, height: 20 },
+      { type: "goal", x: 650, y: 350 },
+      { type: 'text', x: 390, y: 500, text: '- Draw here ^ -'}
+    ]
+  },
+  { // high ground
+    objects: [
+      { type: "start", x: 50, y: 50 },
+      { type: "ground", x: 160, y: 100, width: 300, height: 20 },
+      { type: "ground", x: 650, y: 400, width: 150, height: 20 },
+      { type: "goal", x: 650, y: 350 },
+      { type: 'text', x: 300, y: 250, text: '- And here ^ -'}
+    ]
+  },
+  { // elevator needed
+    objects: [
+      { type: "start", x: 50, y: 0 },
+      { type: "ground", x: 160, y: 100, width: 300, height: 20 },
+      { type: "ground", x: 160, y: 600, width: 100, height: 20 },
+      { type: "goal", x: 160, y: 550 },
+    ]
+  },
+  { // loop
+    objects: [
+      { type: "start", x: 350, y: 100 },
+      { type: "ground", x: 450, y: 300, width: 550, height: 20, angle: Math.PI/4 },
+      { type: "ground", x: 700, y: 510, width: 150, height: 20, angle: Math.PI/8 },
+      { type: "ground", x: 950, y: 540, width: 450, height: 20, angle: Math.PI },
+      { type: "ground", x: 1200, y: 510, width: 150, height: 20, angle: Math.PI*0.75 },
+      { type: "ground", x: 1260, y: 430, width: 100, height: 20, angle: Math.PI*0.65 },
+      { type: "ground", x: 1280, y: 400, width: 100, height: 20, angle: Math.PI*0.55 },
+      { type: "ground", x: 1290, y: 300, width: 300, height: 20, angle: Math.PI*0.5 },
+      { type: "ground", x: 1200, y: 100, width: 150, height: 20, angle: Math.PI*0.2 },
+      { type: "ground", x: 1260, y: 150, width: 100, height: 20, angle: Math.PI*0.25 },
+      { type: "ground", x: 1270, y: 155, width: 100, height: 20, angle: Math.PI*0.35 },
+      { type: "goal", x: 650, y: 350 }
+    ]
+  },
+  { // hill
+    objects: [
+      { type: "start", x: 50, y: 50 },
+      { type: "ground", x: 160, y: 400, width: 400, height: 20 },
+      { type: "ground", x: 550, y: 480, width: 400, height: 20, angle: Math.PI / 8 },
+      { type: "ground", x: 930, y: 750, width: 500, height: 20, angle: Math.PI / 4 },
+      { type: "ground", x: 1320, y: 1300, width: 800, height: 20, angle: Math.PI / 3 },
+      { type: "ground", x: 1700, y: 1840, width: 500, height: 20, angle: Math.PI / 4 },
+      { type: "ground", x: 2260, y: 2180, width: 800, height: 20, angle: Math.PI / 8 },
+      { type: "ground", x: 2840, y: 2340, width: 400, height: 20 },
+      { type: "ground", x: 3090, y: 2325, width: 80, height: 20, angle: -Math.PI / 8 },
+      { type: "ground", x: 3150, y: 2295, width: 40, height: 20, angle: -Math.PI / 4 },
+      { type: "goal", x: 4000, y: 2000 },
+    ]
+  },
+];
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {
 import createGround from './components/ground.js';
 import initLevelManager from './components/levelmanager.js';
 import interact from './lib/interact';
+import levels from './components/levels.js';
 
 window.addEventListener('load', function load() {
   window.removeEventListener('load', load);
@@ -32,49 +33,7 @@ window.addEventListener('load', function load() {
   Engine.run(engine);
   Render.run(render);
 
-  const levels = [
-  {
-    objects: [
-      { type: "start", x: 50, y: 50 },
-      { type: "ground", x: 160, y: 400, width: 400, height: 20 },
-      { type: "ground", x: 700, y: 400, width: 350, height: 20 },
-      { type: "goal", x: 650, y: 350 },
-      { type: 'text', x: 390, y: 500, text: '- Draw here ^ -'}
-    ]
-  },
-  {
-    objects: [
-      { type: "start", x: 50, y: 50 },
-      { type: "ground", x: 160, y: 100, width: 300, height: 20 },
-      { type: "ground", x: 650, y: 400, width: 150, height: 20 },
-      { type: "goal", x: 650, y: 350 },
-      { type: 'text', x: 300, y: 250, text: '- And here ^ -'}
-    ]
-  },
-  {
-    objects: [
-      { type: "start", x: 50, y: 0 },
-      { type: "ground", x: 160, y: 100, width: 300, height: 20 },
-      { type: "ground", x: 160, y: 600, width: 100, height: 20 },
-      { type: "goal", x: 160, y: 550 },
-    ]
-  },
-  {
-    objects: [
-      { type: "start", x: 350, y: 100 },
-      { type: "ground", x: 450, y: 300, width: 550, height: 20, angle: Math.PI/4 },
-      { type: "ground", x: 700, y: 510, width: 150, height: 20, angle: Math.PI/8 },
-      { type: "ground", x: 950, y: 540, width: 450, height: 20, angle: Math.PI },
-      { type: "ground", x: 1200, y: 510, width: 150, height: 20, angle: Math.PI*0.75 },
-      { type: "ground", x: 1260, y: 430, width: 100, height: 20, angle: Math.PI*0.65 },
-      { type: "ground", x: 1280, y: 400, width: 100, height: 20, angle: Math.PI*0.55 },
-      { type: "ground", x: 1290, y: 300, width: 300, height: 20, angle: Math.PI*0.5 },
-      { type: "ground", x: 1200, y: 100, width: 150, height: 20, angle: Math.PI*0.2 },
-      { type: "ground", x: 1260, y: 150, width: 100, height: 20, angle: Math.PI*0.25 },
-      { type: "ground", x: 1270, y: 155, width: 100, height: 20, angle: Math.PI*0.35 },
-      { type: "goal", x: 650, y: 350 }
-    ]
-  }];
+
 
   const { reset } = initLevelManager(engine, render, levels);
   resetButton.addEventListener('click', reset);
